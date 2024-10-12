ce57a766c9626ababd08f6798e0939d18b01dd12
shahryar toulabi <shahryartoolabi61@gmail.com>
2024-10-12 12:21:04 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    content: ["./public/**/*.html"],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ["Vazirmatn"]
+        }
+      },
+    },
+    plugins: [],
+  }
